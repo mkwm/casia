22317b51e3b4eba80535998133e1c6a90e36f4a5
Mateusz Małek <mkwm@users.noreply.github.com>
2013-07-30 11:47:47 +0200

    Add contrib module - boolean serializer which uses yes/no

---
+++ casia/contrib/serializers.py
@@ -0,0 +1,7 @@
+from django.utils.translation import ugettext_lazy as _
+
+from casia.core.serializers import BooleanFieldSerializer
+
+class YesNoBooleanFieldSerializer(BooleanFieldSerializer):
+    TRUE = _('yes')
+    FALSE = _('no')
