ff8717daee34e0667c1a7aa2fce48cb132b37974
Mateusz Małek <mkwm@users.noreply.github.com>
2013-10-03 22:33:51 +0200

    Initial commit - new Django project

---
+++ casia/conf/global_settings.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of Casia - CAS server based on Django
+# Copyright (C) 2013 Mateusz Małek
+
+# Casia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Casia. If not, see <http://www.gnu.org/licenses/>.
+
+# Default Casia settings. Override these with settings in the module
+# pointed-to by the DJANGO_SETTINGS_MODULE environment variable.
+
+TIME_ZONE = 'UTC'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'casia.conf.urls'
+
+WSGI_APPLICATION = 'casia.core.wsgi.application'
---
+++ casia/conf/urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of Casia - CAS server based on Django
+# Copyright (C) 2013 Mateusz Małek
+
+# Casia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Casia. If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf.urls import patterns
+
+urlpatterns = patterns('',
+)
